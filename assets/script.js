19bb1aa5d4da340705dd529f7b06619984d16796
Austin Cudworth <austincudworth@gmail.com>
2020-05-03 17:59:47 -0700

    Add gameController, displayController, and new gameBoard methods

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,143 @@
-//TODO: GAMEBOARD MODULE
+const newPlayer = function(symbol){
+    const mark = symbol;
+    const record = [0, 0, 0];
+
+    const addWin = () => record[0] += 1;
+    const addLoss = ()=> record[1] += 1;
+    const addTie = ()=> record[2] += 1;
+
+    const getRecord = () => console.log(`player ${mark}'s record: ${record[0]} - ${record[1]} - ${record[2]}`);
+
+    return {mark,
+            addWin,
+            addLoss,
+            addTie,
+            getRecord}
+}
+
+
 const gameBoard = (function(){
-    const gameboard = [[0, 0, 0],[0, 0, 0],[0, 0, 0]];
-    return {gameboard}
+    const board = [['','',''],['','',''],['','','']];
+
+    const addMark = function(mark, [i, j]){
+        board[i][j] = mark;
+    };
+
+    const checkWinCondition = function(){
+        return false; //TODO
+    }
+
+    const checkTieCondition = function(){
+        return false; //TODO
+    }
+
+    return {board,
+            addMark,
+            checkWinCondition,
+            checkTieCondition,
+            };
 })();
 
 
-//TODO: GAME LOGIC MODULE
-
-
-//TODO: PLAYER FACTORY FUNCTION
-
-
-//TODO: DISPLAY CONTROLLER MODULE
 const displayController = (function(){
-    const body = document.querySelector('body');
-    gameboard = document.createElement('div');
-    
+    let parent_node;
 
-    return{render}
+    const disp_id = Math.floor(Math.random()*100000);
+
+    game_board = document.createElement('div');
+    game_board.setAttribute('id', `_${disp_id}`);
+    game_board.setAttribute('class', 'gameboard');
+
+    setParent = function(parent){
+        parent_node = parent;
+        parent_node.append(game_board);
+    }
+
+    render = function(){
+
+        game_board.innerHTML = '';
+
+        for (i = 0; i < 3; i++){
+            row = document.createElement('div');
+            row.setAttribute('id',`_${i}_${disp_id}`);
+            row.setAttribute('class','row')
+
+            for (j = 0; j < 3; j++){
+                cell = document.createElement('span');
+                cell.setAttribute('id',`_${i}_${j}_${disp_id}`);
+                cell.setAttribute('class','cell');
+                cell.textContent = gameBoard.board[i][j];
+                cell.addEventListener('click', function(e){
+                    const [i, j] = e.target.id.split('_').slice(1,3);
+                    gameController.addMark([i, j]);
+                })
+                row.append(cell);
+            }
+            game_board.append(row);
+        }
+    }
+
+    return{render,
+            setParent
+        }
 })();
 
-console.log(gameBoard.gameboard[0][0]);
+
+const gameController = (function(){
+    let p1, p2, active_player, inactive_player;
+
+    const newGame = function(){
+        p1 = newPlayer('X');
+        p2 = newPlayer('O');
+        
+        active_player = [p1, p2][Math.round(Math.random())];
+        (p1 == active_player)? inactive_player = p2 : inactive_player = p1;
+
+        displayController.setParent(document.querySelector('body'));
+        displayController.render();
+    }
+
+    const getActivePlayer = function(){
+        return active_player;
+    }
+
+    const swapActivePlayer = function(){
+        if (p1 == active_player) {
+            active_player = p2;
+            inactive_player = p1;
+        } else {
+            active_player = p1;
+            inactive_player = p2;
+        }
+    }
+
+    const addMark = function([i, j]){
+        if ('' != gameBoard.board[i][j]){
+            return;
+        } else {
+            gameBoard.board[i][j] = active_player.mark;
+
+            if (gameBoard.checkWinCondition()) {
+                active_player.addWin();
+                inactive_player.addLoss();
+                console.log(`player ${active_player.mark} is victorious`)
+            }
+
+            if (gameBoard.checkTieCondition()) {
+                active_player.addTie();
+                inactive_player.addTie();
+                console.log(`game resulted in a tie`)
+            }
+
+            swapActivePlayer();
+            displayController.render();
+        }
+    }
+
+    return{newGame,
+            getActivePlayer,
+            addMark};
+})();
+
+
+gameController.newGame();
